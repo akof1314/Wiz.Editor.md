f3e4ae426d1bcb307e03ce7e55ce2335483bfb3c
WuHuan <kof1234@sina.com>
2015-04-21 13:34:39 +0800

    将预览里面的链接到新标签打开

--- Editor.md/editormd.js
+++ Editor.md/editormd.js
@@ -3260,14 +3260,14 @@
                     });
 
                     text = text.replace(atLinkReg, function($1, $2) {
-                        return "<a href=\"" + editormd.urls.atLinkBase + "" + $2 + "\" title=\"&#64;" + $2 + "\" class=\"at-link\">" + $1 + "</a>";
+                        return "<a target=\"_blank\" href=\"" + editormd.urls.atLinkBase + "" + $2 + "\" title=\"&#64;" + $2 + "\" class=\"at-link\">" + $1 + "</a>";
                     }).replace(/_#_&#64;_#_/g, "@");
                 }
                 
                 if (settings.emailLink)
                 {
                     text = text.replace(emailLinkReg, function($1, $2, $3, $4, $5) {
-                        return (!$2 && $.inArray($5, "jpg|jpeg|png|gif|webp|ico|icon|pdf".split("|")) < 0) ? "<a href=\"mailto:" + $1 + "\">"+$1+"</a>" : $1;
+                        return (!$2 && $.inArray($5, "jpg|jpeg|png|gif|webp|ico|icon|pdf".split("|")) < 0) ? "<a target=\"_blank\" href=\"mailto:" + $1 + "\">"+$1+"</a>" : $1;
                     });
                 }
 
@@ -3291,7 +3291,7 @@
                 }
             }
 
-            var out = "<a href=\"" + href + "\"";
+            var out = "<a target=\"_blank\" href=\"" + href + "\"";
             
             if (atLinkReg.test(title) || atLinkReg.test(text))
             {
