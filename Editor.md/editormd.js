9c389c11095220aa90cc9a98a8994200236f3a6f
WuHuan <kof1234@sina.com>
2016-09-10 09:50:27 +0800

    修复右对齐问题和链接下方间距问题

--- Editor.md/editormd.js
+++ Editor.md/editormd.js
@@ -1191,6 +1191,11 @@
             }
 
             toolbarMenu.html(menu);
+            var rlist = toolbarMenu.find("li.pull-right").detach();
+            if (rlist)
+            {
+                rlist.appendTo(toolbarMenu);
+            }
 
             toolbarMenu.find("[title=\"Lowercase\"]").attr("title", settings.lang.toolbar.lowercase);
             toolbarMenu.find("[title=\"ucwords\"]").attr("title", settings.lang.toolbar.ucwords);
