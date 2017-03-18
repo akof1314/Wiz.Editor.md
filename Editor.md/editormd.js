59dbd176c73989a4aff69f5946b6058066de450d
WuHuan <kof1234@sina.com>
2017-03-18 21:04:22 +0800

    添加编辑器新主题

--- Editor.md/editormd.js
+++ Editor.md/editormd.js
@@ -4304,20 +4304,25 @@
     // CodeMirror / editor area themes
     // @1.5.0 rename -> editorThemes, old version -> themes
     editormd.editorThemes = [
-        "default", "3024-day", "3024-night",
+        "default", "3024-day", "3024-night", "abcdef",
         "ambiance", "ambiance-mobile",
-        "base16-dark", "base16-light", "blackboard",
-        "cobalt",
+        "base16-dark", "base16-light", "bespin", "blackboard",
+        "cobalt", "colorforth",
+        "dracula", "duotone-dark", "duotone-light",
         "eclipse", "elegant", "erlang-dark",
-        "lesser-dark",
-        "mbo", "mdn-like", "midnight", "monokai",
+        "hopscotch",
+        "icecoder", "isotope",
+        "lesser-dark", "liquibyte",
+        "material", "mbo", "mdn-like", "midnight", "monokai",
         "neat", "neo", "night",
-        "paraiso-dark", "paraiso-light", "pastel-on-dark",
-        "rubyblue",
-        "solarized",
-        "the-matrix", "tomorrow-night-eighties", "twilight",
+        "panda-syntax", "paraiso-dark", "paraiso-light", "pastel-on-dark",
+        "railscasts", "rubyblue",
+        "seti", "solarized",
+        "the-matrix", "tomorrow-night-bright", "tomorrow-night-eighties", "ttcn", "twilight",
         "vibrant-ink",
-        "xq-dark", "xq-light"
+        "xq-dark", "xq-light",
+        "yeti",
+        "zenburn"
     ];
 
     editormd.loadPlugins = {};
